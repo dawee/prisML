792699c989de19fac3ade077fe23ff27e2c005a5
David Corticchiato <david.corticchiato@gmail.com>
2019-04-22 12:20:34 -0400

    add build script to generate ocamlformat.exe

---
+++ packages/ocamlformat-build/build.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+const util = require("util");
+const { sync: which } = require("which");
+
+const version = "0.9";
+
+const pfs = {
+  copyFile: util.promisify(fs.copyFile),
+  exists: util.promisify(fs.exists)
+};
+
+const esyPath = which("esy");
+const clonePath = path.resolve(__dirname, `_build/ocamlformat`);
+
+const builtEXEPath = path.resolve(
+  clonePath,
+  "_build/default/src/ocamlformat.exe"
+);
+
+function runCMD(cmd, params, options) {
+  return new Promise(done => {
+    const stream = spawn(cmd, params, options);
+
+    stream.stdout.pipe(process.stdout);
+    stream.stderr.pipe(process.stderr);
+    stream.on("close", done);
+  });
+}
+
+async function clone() {
+  const exists = await pfs.exists(clonePath);
+
+  return (
+    !exists &&
+    runCMD("git", ["clone", "git@github.com:ocaml-ppx/ocamlformat", clonePath])
+  );
+}
+
+function patchPackage() {
+  const packageSrc = path.resolve(__dirname, "patch/package.json");
+  const packageDest = path.resolve(`${clonePath}/package.json`);
+
+  return pfs.copyFile(packageSrc, packageDest);
+}
+
+function checkout() {
+  return runCMD("git", ["checkout", "-b", "prisml", version], {
+    cwd: clonePath
+  });
+}
+
+function install(command) {
+  return runCMD(esyPath, ["install"], { cwd: clonePath });
+}
+
+function build(command) {
+  return runCMD(esyPath, ["build", "--release"], { cwd: clonePath });
+}
+
+function exportEXE(outputEXEPath) {
+  return pfs.copyFile(builtEXEPath, outputEXEPath);
+}
+
+async function run(outputEXEPath) {
+  await clone();
+  await checkout();
+  await patchPackage();
+  await install();
+  await build();
+  await exportEXE(outputEXEPath);
+}
+
+if (require.main === module) {
+  run(process.argv[2]);
+}
