223850bcdae50f842af733b9e89b89078b72a353
David Corticchiato <david.corticchiato@ubisoft.com>
2019-04-23 16:58:04 -0400

    fix inverted platforms

--- packages/ocamlformat-prebuilt-mac/prepare.js
+++ packages/ocamlformat-prebuilt-mac/prepare.js
@@ -1,4 +1,4 @@
 const path = require ('path');
 const build = require ('../ocamlformat-build');
 
-build ('0.9', 'win32', path.resolve (__dirname, 'ocamlformat.exe'));
+build ('0.9', 'darwin', path.resolve (__dirname, 'ocamlformat.exe'));
