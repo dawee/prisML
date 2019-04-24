1508ec7dd0130bfae5724928bf6e0519ba31e2aa
David Corticchiato <david.corticchiato@gmail.com>
2019-04-24 00:12:44 -0400

    create prebuilt package for refmt for mac

---
+++ packages/refmt-prebuilt-mac/prepare.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const build = require("../refmt-build");
+
+build(
+  "1483691015a6404317bbcadb0e738cb1adb2b713",
+  "darwin",
+  path.resolve(__dirname, "refmt.exe")
+);
